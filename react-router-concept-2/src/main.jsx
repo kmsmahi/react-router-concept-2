521a272894bab603962793ee0f8f2b3d62d998fe
kmsmahi <salauddinmahi2002@gmail.com>
2026-04-27 12:19:02 +0600

    initial setup

--- react-router-concept-2/src/main.jsx
+++ react-router-concept-2/src/main.jsx
@@ -2,9 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import Contact from './components/Contact/Contact.jsx';
+import About from './components/About/About.jsx';
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<App></App>
+  },
+  {
+    path:'/contact',
+    element:<Contact></Contact>
+  },
+  {
+    path:'/about',
+    element:<About></About>
+  }
 
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
